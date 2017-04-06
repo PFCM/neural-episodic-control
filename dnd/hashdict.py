998fa33d49aa29f16268f0a20fc618ba9a2ce27f
Paul Mathews <pfcmathews@gmail.com>
2017-04-06 12:18:22 +1200

    storing values successfully, although still need to determine appropriate behaviour when full. Least-recently-used has been done a few times in the literature, but would take a bit of extra accounting which might be a pain. At the same time, it isn't clear what a viable alternative would be. Currently it is chosen at random, which doesn't seem like a great plan in the long run

--- dnd/hashdict.py
+++ dnd/hashdict.py
@@ -12,29 +12,34 @@ Problems with this approach:
 import numpy as np
 import tensorflow as tf
 
-from dnd.lsh import simhash
+from dnd.lsh import simhash, get_simhash_config
 
 
 class HashDND(object):
     """differentiable neural dictionary, using LSH for approximate
     nearest neighbour lookup. Assumes keys are vectors. Also assumes we only
-    use float32, for now."""
+    use float32 and doesn't handle batched operations :("""
 
-    sentinal_value = np.inf
+    sentinel_value = np.inf
 
     @classmethod
-    def _setup_values(cls, hash_bits, max_neighbours, value_shapes):
+    def _setup_variables(cls, hash_bits, max_neighbours, key_size,
+                         value_shapes):
         """setup variables with appropriate initializers given the shapes"""
-        values = []
         init = tf.constant_initializer(cls.sentinel_value)
+
+        keys = tf.get_variable(name='keys',
+                               shape=[2**hash_bits * max_neighbours, key_size],
+                               initializer=init)
+
+        values = []
         for i, shape in enumerate(value_shapes):
-            var_shape = [2**hash_bits, max_neighbours] + shape
+            var_shape = [2**hash_bits * max_neighbours] + shape
             var = tf.get_variable(name='value_{}'.format(i),
                                   shape=var_shape,
                                   initializer=init)
             values.append(var)
-        return values
-
+        return keys, values
 
     def __init__(self, hash_bits, max_neighbours, key_size, value_shapes):
         """Set up the dnd.
@@ -52,20 +57,34 @@ class HashDND(object):
             value_shapes ()
         """
         self._hash_size = hash_bits
-        self._keys = tf.zeros([2**hash_bits, max_neighbours, key_size])
-        self._values = HashDND._setup_values(hash_bits,
-                                             max_neighbours,
-                                             value_shapes)
-        self._hash_config = get_simhash_config(self.key_size,
+        self._key_size = key_size
+        self._bucket_size = max_neighbours
+        self._keys, self._values = HashDND._setup_variables(hash_bits,
+                                                            max_neighbours,
+                                                            key_size,
+                                                            value_shapes)
+        self._hash_config = get_simhash_config(self._key_size,
                                                self._hash_size)
 
+        self._summarise_pressure()
+
+    def _summarise_pressure(self):
+        """add summaries for the load. It would be nice to have this
+        per-bucket, but there is potentially a lot of buckets."""
+        with tf.name_scope('dnd_stats'):
+            used_keys = tf.not_equal(self._keys[:, 0], self.sentinel_value)
+            filled_keys = tf.reduce_sum(tf.cast(used_keys, tf.float32))
+            tf.summary.scalar('total_fill', filled_keys)
+
     def _get_bucket(self, key):
-        """look up the keys and values in a given bucket correspondng to the
-        hash of the given key"""
-        bucket_idx = simhash(key, self._hash_config)
-        keys = self._keys[bucket_idx, ...]
-        values = [val[bucket_idx, ...] for val in self._values]
-        return keys, values
+        """look up the contents of a bucket by hash. Also return the bucket
+        index so we can create updates to the sotrage variables."""
+        idx = simhash(key, self._hash_config)
+        bucket_start = idx * self._bucket_size
+        bucket_end = (idx + 1) * self._bucket_size
+        keys = self._keys[bucket_start:bucket_end, ...]
+        values = [val[bucket_start:bucket_end, ...] for val in self._values]
+        return keys, values, idx
 
     def store(self, key, value):
         """Gets an op which will store the key-value pair. This involves the
@@ -80,22 +99,79 @@ class HashDND(object):
                 - TODO: figure this out (LRU?)
 
         Args:
-            key (tensor): `[batch_size, key_size]` batch of keys
-            value (tensor or list of tensors): `[batch_size, ???]` tensors to
+            key (tensor): `[key_size]` key to store
+            value (list of tensors): `[???]` tensors to
                 be stored.
 
         Returns:
             op: an op which carries out the above steps.
         """
         with tf.name_scope('dnd/store'):
-            bucket_keys, bucket_values = self._get_bucket(key)
+            bucket_keys, bucket_values, idx = self._get_bucket(key)
             # is there space?
-            bucket_full = tf.reduce_all(
-                tf.stack(
-                    [tf.reduce_all(tf.not_equal(var, self.sentinel_value))
-                     for var in bucket_values]))
-            # if there is, find it and assign to it
-            # if there is not, do something else??
+            can_store = tf.reduce_any(tf.equal(bucket_keys[:, 0],
+                                               self.sentinel_value))
+            can_store = tf.Print(can_store, [can_store])
+
+            def _empty_store():
+                return self._get_store_op_empty(key, value, idx, bucket_keys)
+
+            def _full_store():
+                return self._get_store_op_full(key, value, idx, bucket_keys)
+
+            store_op = tf.cond(can_store, _empty_store, _full_store)
+        return store_op
+
+    def _flatten_index(self, index, bucket_index):
+        """turn a bucket-level index into a global index"""
+        return index + (bucket_index * self._bucket_size)
+
+    def _update_at_index(self, index, new_key, new_vals):
+        """make update ops to insert at the appropriate (flattened) index"""
+        # update the keys
+        key_update = tf.scatter_update(self._keys, [index],
+                                       tf.expand_dims(new_key, 0))
+        # and update the values
+        value_updates = []
+        for value_var, new_val in zip(self._values, new_vals):
+            val_update = tf.scatter_update(value_var, [index],
+                                           tf.expand_dims(new_val, 0))
+            value_updates.append(val_update)
+
+        # make sure they all happen at once
+        return tf.group(key_update, *value_updates)
+
+    def _get_store_op_empty(self, store_key, store_vals, bucket_index,
+                            bucket_keys):
+        """get an op to store given key and values in the first empty space.
+
+        Returns an op with no output that will run all of the required updates.
+        """
+        # first find the first empty spot (assuming there is one)
+        with tf.name_scope('empty_store'):
+            empty_indices = tf.where(tf.equal(bucket_keys[:, 0],
+                                              self.sentinel_value))
+            empty_indices = tf.cast(empty_indices, tf.int32)
+            store_idx = self._flatten_index(empty_indices[0, 0], bucket_index)
+            return self._update_at_index(store_idx, store_key, store_vals)
+
+    def _get_store_op_full(self, store_key, store_vals, bucket_index,
+                           bucket_keys):
+        """get an op to store given keys and values when there are no empty
+        slots.
+
+        Returns an op with no output that will run all of the require updates.
+        """
+        # TODO: what should this do? LRU? Need some accounting for that,
+        # otherwise some kind of interpolation for lossily storing it in the
+        # bucket?
+        # for now we are just going to choose at random, which is surely a
+        # terrible strategy, but at least it will run
+        with tf.name_scope('store_full'):
+            idx = tf.random_uniform([], minval=0, maxval=self._bucket_size,
+                                    dtype=tf.int32)
+            store_idx = self._flatten_index(idx, bucket_index)
+            return self._update_at_index(store_idx, store_key, store_vals)
 
     def _get_averaged_value(self, bucket, values, similarities):
         """get the values from a specific bucket, weighted by similarities and
--- dnd/test_dnd.py
+++ dnd/test_dnd.py
@@ -1,18 +1,85 @@
 """tests for the dnd"""
+import numpy as np
 import tensorflow as tf
 
 from tensorflow.python.platform import test
 
-import dnd.hashdict
+import dnd.hashdict as hashdict
+
+
+def _row_in(row, mat):
+    """check a row is in a matrix"""
+    return np.any(np.equal(mat, row).all(1))
 
 
 class TestDND(test.TestCase):
 
     simple_shapes = [[100]]
 
-    def test_store(self):
+    def test_store_empty(self):
         """make sure we can store values in the dictionary"""
-        self.assertTrue(False)
+        dictionary = hashdict.HashDND(4, 10, 5, TestDND.simple_shapes)
+
+        key = tf.get_variable('key', shape=[5])
+        value = tf.get_variable('value', shape=TestDND.simple_shapes[0])
+
+        store_op = dictionary.store(key, [value])
+
+        # now there should be non-infs in the dicts key/value vars
+        key_stored = tf.reduce_any(tf.not_equal(dictionary._keys,
+                                                dictionary.sentinel_value))
+        val_stored = tf.reduce_any(tf.not_equal(dictionary._values[0],
+                                                dictionary.sentinel_value))
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            # run the store op
+            sess.run(store_op)
+
+            self.assertTrue(sess.run(key_stored))
+            self.assertTrue(sess.run(val_stored))
+
+    def test_store_full(self):
+        """make sure stuff keeps going in even when it is full up."""
+        # NOTE: what is supposed to happen in this case is not settled, so this
+        # is likely to need to be redone
+        dictionary = hashdict.HashDND(1, 1, 5, TestDND.simple_shapes)
+
+        # just use random keys to make sure we cover everything
+        key = tf.random_normal([5])
+        # random values as well
+        value = tf.random_normal(TestDND.simple_shapes[0])
+
+        keys_full = tf.reduce_all(tf.not_equal(dictionary._keys,
+                                               dictionary.sentinel_value))
+        vals_full = tf.reduce_all(tf.not_equal(dictionary._keys,
+                                               dictionary.sentinel_value))
+
+        store_op = dictionary.store(key, [value])
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            # there are 2 buckets, each with a single slot
+            # but there are likely to be collisions, so it may take a few to
+            # fill up
+            for _ in range(4):
+                _, newkey, newval, allkeys, allvals = sess.run(
+                    [store_op, key, value, dictionary._keys] +
+                    dictionary._values)
+                self.assertTrue(_row_in(newkey, allkeys))
+                self.assertTrue(_row_in(newval, allvals))
+            # check that it is full
+            self.assertTrue(sess.run(keys_full), '{}'.format(allkeys))
+            self.assertTrue(sess.run(vals_full))
+
+            # add another one
+            _, newkey, newval, allkeys, allvals = sess.run(
+                [store_op, key, value, dictionary._keys] +
+                dictionary._values)
+            self.assertTrue(_row_in(newkey, allkeys))
+            self.assertTrue(_row_in(newval, allvals))
 
     def test_get(self):
         """make sure we can pull things back out"""
